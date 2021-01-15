0b56969d8d6823f8f1f5e78bbb7dd7a0c89eef71
IndustrieCreative <industrie.creative@gmail.com>
2021-01-15 20:45:22 +0100

    FIXED: url

--- base_view_dynamic_inheritance/__manifest__.py
+++ base_view_dynamic_inheritance/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """A technical module to load inherited views dynamically.""",
     'license': 'OPL-1',
     'author': 'Walter Mantovani',
-    'website': 'https://github.com/IndustrieCreative',
+    'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     'version': '12.0.1.0.0',
     'depends': ['base'],
