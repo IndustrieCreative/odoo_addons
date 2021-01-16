b2749d26d1a3d5fc8f3b5a8f13bf70809dbd3b90
IndustrieCreative <industrie.creative@gmail.com>
2021-01-16 07:38:54 +0100

    UPDATED: licence correction

--- base_view_dynamic_inheritance/__manifest__.py
+++ base_view_dynamic_inheritance/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Dynamic views inheritance',
     'summary': """A technical module to load inherited views dynamically.""",
     'description': """A technical module to load inherited views dynamically.""",
-    'license': 'OPL-1',
+    'license': 'AGPL-3',
     'author': 'Walter Mantovani',
     'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
--- ir_attachment_orphan_gc/__manifest__.py
+++ ir_attachment_orphan_gc/__manifest__.py
@@ -3,6 +3,7 @@
     'name': 'M2M and M2O orphan Attachments garbage collector',
     'summary': 'A garbage collector for orphan attachments from Many2many and Many2one fields.',
     'description': '',
+    'license': 'AGPL-3',
     'author': 'Walter Mantovani',
     'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
