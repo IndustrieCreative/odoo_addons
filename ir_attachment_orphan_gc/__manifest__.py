2766ef4fda8cd49b855d71a0b57d31897b08be70
IndustrieCreative <industrie.creative@gmail.com>
2021-01-18 16:29:52 +0100

    UPDATED: incremented version number

--- ir_attachment_orphan_gc/__manifest__.py
+++ ir_attachment_orphan_gc/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Walter Mantovani',
     'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'depends': ['base'],
     'application': False,
     'data': [
