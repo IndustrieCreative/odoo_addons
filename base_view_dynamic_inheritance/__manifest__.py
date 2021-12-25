e51f8bdbb86f5d73cd0245dd95f9bb3f371af220
IndustrieCreative <industrie.creative@gmail.com>
2021-12-25 19:47:39 +0100

    UPGRADED: first changes for v12 to v13 migration

--- base_view_dynamic_inheritance/__manifest__.py
+++ base_view_dynamic_inheritance/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Walter Mantovani',
     'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': ['base'],
     'application': False,
     'data': ['security/asc_base_security.xml'],
