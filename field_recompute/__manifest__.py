ea4fbb302c56b7a7b32bf0f1239f9779d2a3a744
IndustrieCreative <industrie.creative@gmail.com>
2022-01-03 03:59:01 +0100

    UPGRADED: increment version

--- field_recompute/__manifest__.py
+++ field_recompute/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Walter Mantovani',
     'website': 'https://github.com/IndustrieCreative/odoo_addons',
     'category': 'Technical Settings', # https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'depends': ['base'],
     'application': False,
     'data': [
